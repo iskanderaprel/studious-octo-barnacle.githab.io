26826346f5bbab53605cf07acb0b42b4204baf5b
iskanderaprel <129663481+iskanderaprel@users.noreply.github.com>
2023-04-08 20:37:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const burgerEl = document.querySelector('.burger');
+const menuEl = document.querySelector('.menu');
+burgerEl.addEventListener('click', function (e) {
+  menuEl.classList.toggle('menu_show');
+}); 
+
+
+
+const login__popapEl = document.querySelector('.login__popap');
+const button_enterEl = document.querySelector('.button_enter');
+
+button_enterEl.addEventListener('click', function (e) {
+login__popapEl.classList.toggle('visible');
+})
